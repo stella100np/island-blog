c15f4052297548afd0dcd0642bab4194d5b868be
gyzeng <gyzeng@adjacentech.com>
2022-08-12 13:44:57 +0800

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://vercel.com',
+  integrations: [mdx(), tailwind(), react(), vue(), sitemap()],
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  }
+});
