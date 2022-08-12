8eba702c21544ce55c831b08ada80e224958dc20
gyzeng <gyzeng@adjacentech.com>
2022-08-12 17:58:44 +0800

    ✨ feat: home & post pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vercel.com',
-  integrations: [mdx(), tailwind(), react(), vue(), sitemap()],
+  integrations: [mdx(), tailwind(), react(), vue(), sitemap(), image()],
   vite: {
     ssr: {
       external: ["svgo"]
---
+++ src/data/siteMetadata.js
@@ -0,0 +1,13 @@
+const siteMetadata = {
+  title: 'stella100np`s Advanced FE blog',
+  author: 'Takasaki Chidori',
+  headerTitle: 'Advanced FE blog',
+  description: 'All the latest web tech, straight from the advanced front-end engineer.',
+  language: 'zh-CN',
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteRepo: 'https://github.com/stella100np/nextjs-blog',
+  siteLogo: '/static/images/logo.png',
+  image: '/TakasakiChidori.jpg',
+
+}
+export default siteMetadata
