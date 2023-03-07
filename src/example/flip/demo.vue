cf46fefcad1640945209375b95c15747debc8553
gyzeng <2744636484@qq.com>
2023-03-07 21:36:36 +0800

    🔧 build(pages): sandpack2

--- src/example/flip/demo.vue
+++ src/example/flip/demo.vue
@@ -1,53 +1,45 @@
 <template>
-  <main id="app">
-    <h4>a FLIP demo of vue3</h4>
-    <section>
-      <ul>
-        <ol @click="setCurrent('Blues')">
-          Blues
-          <div id="Blues" class="line" v-if="current == 'Blues'"></div>
-        </ol>
-        <ol @click="setCurrent('Rock')">
-          Rock
-          <div id="Rock" class="line" v-if="current == 'Rock'"></div>
-        </ol>
-        <ol @click="setCurrent('Jazz')">
-          Jazz
-          <div id="Jazz" class="line" v-if="current == 'Jazz'"></div>
-        </ol>
-      </ul>
-    </section>
-  </main>
+<main id="app">
+  <h4>a FLIP demo of vue3</h4>
+  <section>
+    <ul>
+      <ol @click="setCurrent('Blues')">
+        Blues
+        <div id="Blues" class="line" v-if="current == 'Blues'"></div>
+      </ol>
+      <ol @click="setCurrent('Rock')">
+        Rock
+        <div id="Rock" class="line" v-if="current == 'Rock'"></div>
+      </ol>
+      <ol @click="setCurrent('Jazz')">
+        Jazz
+        <div id="Jazz" class="line" v-if="current == 'Jazz'"></div>
+      </ol>
+    </ul>
+  </section>
+</main>
 </template>
 
-<script>
+<script setup>
 import { ref, nextTick } from "vue";
-export default {
-  name: "App",
-  setup() {
-    const current = ref("Blues");
-    const setCurrent = async (v) => {
-      let oldElm = document.getElementById(current.value);
-      let first = oldElm.getBoundingClientRect();
-      current.value = v;
-      //NOTICE: After this, dom was changed.
-      await nextTick();
-      let newElm = document.getElementById(current.value);
-      let last = newElm.getBoundingClientRect();
-      const invert = first.top - last.top;
-      newElm.animate(
-        [
-          { transform: `translateY(${invert}px)` },
-          { transform: "translateY(0)" },
-        ],
-        {
-          duration: 300,
-          easing: "cubic-bezier(0,0,0.32,1)",
-        }
-      );
-    };
-    return { current, setCurrent };
-  },
+
+const current = ref("Blues");
+const setCurrent = async (v) => {
+  let oldElm = document.getElementById(current.value);
+  let first = oldElm.getBoundingClientRect();
+  current.value = v;
+  //NOTICE: After this, dom was changed.
+  await nextTick();
+  let newElm = document.getElementById(current.value);
+  let last = newElm.getBoundingClientRect();
+  const invert = first.top - last.top;
+  newElm.animate(
+    [{ transform: `translateY(${invert}px)` }, { transform: "translateY(0)" }],
+    {
+      duration: 300,
+      easing: "cubic-bezier(0,0,0.32,1)",
+    }
+  );
 };
 </script>
 
--- src/example/flip/index.js
+++ src/example/flip/index.js
@@ -1,4 +1,4 @@
-import framerjs from "./framer?raw"
+import framerjs from "./framer.jsx?raw"
 import framercss from "./framer.css?raw"
 
 export {
